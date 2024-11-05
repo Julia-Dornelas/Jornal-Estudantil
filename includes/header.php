e0fbb798fe7507fa50d7e39b7c6b23234f1798c2
Júlia Dornelas <129866827+Julia-Dornelas@users.noreply.github.com>
2024-11-05 10:40:21 -0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+    <ul class="list-group list-group-flush">
+            <li class="list-group-item text-white bg-dark">
+              <a class="badge-dark" href="index.html">Home</a>
+            </li>
+            <li class="list-group-item text-white bg-dark">
+              <a class="badge-dark" href="catalogo.html">Notícias</a>
+            </li>
+            <li class="list-group-item text-white bg-dark">
+              <a class="badge-dark" href="catalogocolecionador.html">Voz do Estudante</a>
+            </li>
+            <li class="list-group-item text-white bg-dark">
+              <a class="badge-dark" href="sobre.html">Institucional</a>
+            </li>
+            <li class="list-group-item text-white bg-dark">
+              <a class="badge-dark" href="contatos.html">Sobre o IFTM</a>
+            </li>
+          </ul>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</div>
---
+++ vozdoestudante/vozdoestudante.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "../includes/head.php"; ?>
+    <title>Voz do Estudante</title>
+</head>
+
+<body>
+    <?php include "../includes/header.php"; ?>
+aaaaaa
+    <?php include "../includes/js.php"; ?>
+</body>
+
+</html>
