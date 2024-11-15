952a6358785f9169df89397d74a9a8583e195e1e
Júlia Dornelas <129866827+Julia-Dornelas@users.noreply.github.com>
2024-11-15 19:30:53 -0300

    Add files via upload

---
+++ vozdoestudante/CARD1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "../includes/head.php"; ?>
+    <title>Voz do Estudante</title>
+</head>
+
+<body>
+    <?php include "../includes/header.php"; ?>
+
+    <h4 class="text-center mt-4  font-weight-bold">Voz do Estudante</h4>
+
+    <div class="d-flex flex-wrap p-3">
+        <img class="card-img-top" src="../img-vozdoestudante/Card1.png" alt="Imagem Card 1" />
+    </div>
+
+    <div class="card-body">
+        <p class="card-text text-justify ">“O 1° está sendo uma ótima experiência, sei que o instituto agrega muito na minha carreira profissional, consigo lidar bem com os colegas e todos são pessoas amigáveis e respeitosas, sobre o método de ensino acho de uma qualidade superior e de grande excelência.”</p>
+        <p class="card-text text-right">Matheus Alves Ferreira, 1° CONT 2024</p>
+    </div>
+
+    <?php include "../includes/footer.php"; ?>
+    <?php include "../includes/js.php"; ?>
+</body>
+
+</html>
---
+++ vozdoestudante/gremio.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "../includes/head.php"; ?>
+    <title>Gremio Estudantil</title>
+</head>
+
+<body>
+    <?php include "../includes/header.php"; ?>
+
+    <h4 class="text-center p-3 font-weight-bold">Grêmio Estudantil</h4>
+
+    <div class="p-3">
+        <p class="text-justify"> O Grêmio Estudantil do Instituto Federal do Triângulo Mineiro (IFTM) - Campus Patrocínio é a principal entidade de representação dos estudantes, atuando como um elo entre o corpo discente e a administração do instituto. Composto por uma equipe dedicada de alunos, o grêmio desempenha um papel crucial na promoção dos interesses estudantis, organização de eventos e fortalecimento da comunidade acadêmica.</p>
+        <div class="d-flex flex-wrap">
+            <img class="card-img-top" src="../img-vozdoestudante/FotoGremio.png" alt="Imagem Card 1" />
+        </div>
+        <p class="text-center">Grêmio Ômega 2024</p>
+    </div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+
+    <h6 class="text-center mt-3"><u>Estrutura e composição</u></h6>
+
+    <div class="p-3">
+        <p class="text-justify">O grêmio é formado por diversos cargos, cada um com responsabilidades específicas, garantindo que todas as áreas de interesse dos estudantes sejam atendidas. A composição atual inclui:</p>
+        <p class="m-0">• Presidente</p>
+        <p class="m-0">• Secretário Geral</p>
+        <p class="m-0">• 1° Secretário</p>
+        <p class="m-0">• Tesoureiro Geral</p>
+        <p class="m-0">• 1° Tesoureiro</p>
+        <p class="m-0">• Secretário de Esportes</p>
+        <p class="m-0">• Diretor Sociocultural</p>
+        <p class="m-0">• Diretor de Relações Acadêmicas</p>
+        <p class="m-0">• Diretor de Comunicação</p>
+        <p class="m-0">• 1° Suplente</p>
+        <p class="m-0">• 2° Suplente</p>
+        <p class="m-0">• 3° Suplente</p>
+    </div>
+
+    <!--CARROSEL-->
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=Primeiro Slide" alt="Primeiro Slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Segundo Slide" alt="Segundo Slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Terceiro Slide" alt="Terceiro Slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+        </a>
+    </div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+
+    <h6 class="text-center mt-3"><u>Funções e atividades</u></h6>
+
+    <div class="p-3">
+        <p class="text-justify"> O grêmio tem como missão defender os direitos dos estudantes, promover atividades culturais, esportivas e acadêmicas, além de incentivar a participação ativa dos alunos na vida escolar. As principais atividades incluem:</p>
+        <p>• Representação Estudantil: O grêmio atua como porta-voz dos alunos, levando suas demandas e sugestões à direção do campus e participando de reuniões e conselhos.</p>
+        <p>• Organização de Eventos: Promove eventos culturais, como festivais, palestras e workshops, além de competições esportivas, incentivando a integração e o desenvolvimento pessoal dos estudantes.</p>
+        <p>• Apoio Acadêmico: Oferece suporte aos alunos em questões acadêmicas, colaborando na organização de grupos de estudo e na realização de projetos que visam melhorar o ambiente educacional.</p>
+    </div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <h6 class="text-center mt-3"><u>Processo Eleitoral</u></h6>
+    <div class="p-3">
+        <p class="text-justify"> A eleição para o grêmio estudantil ocorre anualmente, permitindo que qualquer aluno do instituto possa se candidatar. Para isso, é necessário formar uma chapa completa, composta por todos os 12 integrantes. Este processo democrático garante que todos os estudantes tenham a oportunidade de participar ativamente da gestão estudantil e contribuir para a melhoria contínua do ambiente escolar.</p>
+    </div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <h6 class="text-center mt-3"><u>Importância</u></h6>
+    <div class="p-3">
+        <p class="text-justify"> O grêmio estudantil é fundamental para o fortalecimento da comunidade acadêmica, promovendo a participação ativa dos alunos e assegurando que suas vozes sejam ouvidas. Além disso, desempenha um papel vital no desenvolvimento de habilidades de liderança, organização e trabalho em equipe entre seus membros, preparando-os para futuros desafios pessoais e profissionais.</p>
+    </div>
+    <?php include "../includes/footer.php"; ?>
+    <?php include "../includes/js.php"; ?>
+</body>
+
+</html>
