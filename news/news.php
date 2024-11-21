4386c21a31315c18608e1c14965a33b16411ff62
Júlia Dornelas <129866827+Julia-Dornelas@users.noreply.github.com>
2024-11-21 11:35:26 -0300

    Add files via upload

---
+++ news/news.php
@@ -0,0 +1,161 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Noticias</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <<link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Protest+Strike&family=Quicksand&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Protest+Strike&family=Quicksand&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    ]<?php include "../includes/header.php"; ?>
+    <div class="px-5">
+    <div class="h1">
+        <h1>Processos Seletivos</h1>
+    </div>
+    <br>
+    <img src="./img/img1.png" alt="" class="img1 imgpc">
+    <br>
+    <br>
+    <div class="p1 font30">
+        <p1 class="font30"> O processo seletivo do IFTM Campus Patrocínio oferece diversas oportunidades para ingresso em cursos
+            técnicos gratuitos no primeiro semestre de 2025. As inscrições, abertas até 11 de novembro de 2024, são
+            realizadas no site da empresa parceira, Nosso Rumo, com uma taxa de inscrição de R$ 40,00. É possível
+            solicitar isenção para candidatos inscritos no CadÚnico ou de famílias de baixa renda, desde que façam o
+            pedido até 16 de outubro.</p1>
+    </div>
+    <div class="bt d-flex justify-content-center"><a href="https://iftm.edu.br/copese/solicitacaomatricula.php?psId=Nc]yzEzMSCRJ865VN!">
+        <div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35" style="height:11px">Inscreva-se</p></button></div>
+    </a></div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <br>
+    <div class="h2">
+        <h1>Conquistas Acadêmicas</h1>
+    </div>
+    <br>
+    <div class="obg"><h1>Olimpíada Brasileira de Geografia (OBG)</h1></div>
+    <br>
+    <div class="p2 font30">
+        <p class="font30">A Olimpíada Brasileira de Geografia (OBG) é uma competição acadêmica voltada para alunos de ensino médio de
+            todo o Brasil, com foco em temas de geografia física, humana e questões socioambientais. Em 2023, o IFTM
+            Campus Patrocínio teve um desempenho notável nessa competição ao conquistar um total de 57 medalhas na etapa
+            estadual. O campus obteve 12 medalhas de ouro, 21 de prata e 24 de bronze, consolidando-se como uma das
+            instituições com mais destaque na competição.
+        </p>
+    </div>
+    <br>
+
+    <img src="./img/img2.png" alt="" class="img2 imgpc">
+    <br>
+    <div  class="bt d-flex justify-content-center"><div class="bt"  style="height:11px"><a
+        href="https://iftm.edu.br/noticias/2023/09/29/campus-patrocinio-obtem-resultado-expressivo-na-olimpiada-brasilieira-de-geografia/">
+        <div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div>
+    </a></div> </div>
+    <br>
+    <br>
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <br>
+    <div class="h3"><h1>Conquistas Acadêmicas</h1></div>
+    <br>
+    <div class="obmep"><h1 class="obm">Olimpíada Brasileira de Matemática das Escolas Públicas (OBMEP)</h1></div>
+    <br>
+    <div class="p3 font30"><p class="font30">A Olimpíada Brasileira de Matemática das Escolas Públicas (OBMEP) é uma das maiores competições acadêmicas de matemática no Brasil, voltada exclusivamente para estudantes de escolas públicas do ensino fundamental e médio. Organizada pelo Instituto Nacional de Matemática Pura e Aplicada (IMPA). O IFTM Campus Patrocínio tem conquistado várias medalhas ao longo das edições da OBMEP, incluindo uma medalha de prata, que representa um feito significativo devido ao alto nível de dificuldade das provas e ao grande número de participantes em todo o país.</p></div>
+    <br>
+    <img src="./img/img3.png" alt="" class="img3 imgpc">
+    <br>
+    <br>
+    <div  class="bt d-flex justify-content-center"><a href="https://www.jornaldepatrocinio.com/iftm-campus-patrocinio-realiza-entrega-de-premiacoes-de-olimpiadas-de-matematica">
+        <div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div></a></div>
+        <br>
+        <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+        <br>
+        <div class="h3"><h1>Conquistas Acadêmicas</h1></div>
+        <br>
+        <div class="obli">Olimpíada Brasileira de Língua Inglesa (OBLI)</div>
+        <br>
+        <div class="p4 font30"><p class="font30">O IFTM Campus Patrocínio participou pela primeira vez da Olimpíada Brasileira de Língua Inglesa (OBLI) em 2024, que avalia a compreensão de inglês entre estudantes do divensino médio em todo o Brasil. A prova foi realizada online, e os alunos do campus se destacaram com a conquista de uma medalha de ouro e quatro de bronze. A participação na OBLI oferece aos estudantes uma experiência enriquecedora, fortalecendo suas habilidades linguísticas e culturais em inglês, algo cada vez mais valorizado em um mercado globalizado. Essa conquista também reflete o compromisso do campus em proporcionar oportunidades de internacionalização e capacitação em línguas estrangeiras para os alunos</p></div>
+        <br>
+        <img src="./img/img4.png" alt="" class="img4 imgpc">
+        <br>
+        <br>
+        <div  class="bt d-flex justify-content-center"><a href="https://iftm.edu.br/noticias/2024/09/04/estudantes-do-iftm-campus-patrocinio-conquistam-medalhas-na-olimpiada-brasileira-de-lingua-inglesa/"><div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35 "  style="height:11px">Saber mais sobre</p></button></div></a></div>
+
+        <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+        <br>
+
+        <div class="h4"><h1>Eventos e Simpósios que ocorreram 21ª Semana Nacional de Ciência e Tecnologia (SNCT):</h1></div>
+        <br>
+        <br>
+        <div class="p5 font30"><p class="font30">Realizada de 18 a 26 de outubro, a SNCT teve como objetivo promover a reflexão e o diálogo sobre os biomas brasileiros, reunindo a comunidade científica, professores, pesquisadores, estudantes e o público em geral</p></div>
+        <br>
+        <img src="./img/img5.png" alt="" class="img5 imgpc">
+        <br>
+        <br>
+       <div  class="bt d-flex justify-content-center"> <a href="https://iftm.edu.br/eventos/snct2024.ptc/"><div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div></a></div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <br>
+    <div class="h5"><h1>Evento IDEATHON 2024</h1></div>
+    <br>
+    <br>
+    <div class="p6 font30"><p class="font30">Realizada de 18 a 26 de outubro, tivemos a competição do IDEATHON onde os alunos tiveram que desenvolver soluções inovadoras focadas em Inteligência Artificial, sobre o tema: Biomas do Brasil: diversidade, saberes e tecnologias sociais, onde tivemos com vencedores a turma do Terceiro ano de Informática</p></div>
+    <br>
+    <img src="./img/img6.png" alt="" class="img6 imgpc">
+    <br>
+    <br>
+    <div  class="bt d-flex justify-content-center"><a href="https://iftm.edu.br/eventos/snct2024.ptc/"><div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div></a></div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <br>
+    <br>
+    <div class="h6"><h1>Evento Simpósio de Administração e Contabilidade</h1></div>
+    <br>
+    <br>
+    <div class="p7 font30"><p class="font30">Nos dias 02 e 03 de abril, foi realizado, no Campus Patrocínio, o Simpósio de Administração e Contabilidade do IFTM. Com foco em Educação Empreendedora, o evento reuniu profissionais, servidores e discentes com o intuito de discutir e compartilhar conhecimentos sobre a integração do empreendedorismo no contexto educacional, com ênfase nas áreas de Administração e Contabilidade.</p></div>
+    <br>
+    <img src="./img/img7.png" alt="" class="img7 imgpc">
+    <br>
+    <br>
+    <div  class="bt d-flex justify-content-center"><a href="https://iftm.edu.br/noticias/2024/04/04/campus-patrocinio-realiza-i-simposio-de-administracao-e-contabilidade/"><div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div></a></div>
+
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <br>
+    <br>
+    <div class="h7"><h1>Fenabom 2024</h1></div>
+    <br>
+    <br>
+    <div class="p8 font30 "><p class="font30">No dia 23 de agosto, os alunos do 2º ano do Curso Técnico em Contabilidade do Campus Patrocínio, acompanhados pelos professores Guilherme Borges e Regina Staropoli, participaram de uma visita técnica à Feira de Negócios Agropecuários da Bom Negócio Nutrição Animal.
+Na ocasião, além de conhecerem a fábrica de produção de ração da empresa, puderam interagir com profissionais do Agronegócio e assistir à palestra “Perspectivas do mercado agropecuário com foco na pecuária leiteira e grãos”</p></div>
+    <br>
+    <img src="./img/img8.png" alt="" class="img8 imgpc">
+    <br>
+<div  class="bt d-flex justify-content-center"><a href="https://iftm.edu.br/noticias/2024/08/23/estudantes-do-campus-patrocinio-participam-da-fenabom-2024/"><div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div></a></div>
+<br>
+    <div class="mx-3 mt-4" style="border-top: 1px solid black;"></div>
+    <br>
+    <br>
+    <div class="h8"><h1>Fenabom 2024</h1></div>
+    <br>
+    <br>
+    <div class="p9 font30"><p class="font30">Como o IFTM pode promover o uso consciente e eficiente de recursos energéticos? Uma das respostas está na capacitação de seus professores para trabalhar esses temas em seus cursos e unidades. Entre os dias 16 e 20 de setembro, o professor Artur de Almeida Rios, do IFTM Campus Patrocínio, participou do curso de Formação de Formadores na área de Eficiência Energética em Edificações, realizado em Brasília.
+O evento, parte do Projeto EnergIF, reuniu 56 professores de Institutos Federais de todo o Brasil e foi organizado pelo Programa Nacional de Conservação de Energia Elétrica (Procel) e pelo Instituto Federal de Santa Catarina (IFSC).</p></div>
+    <br>
+    <img src="./img/img9.png" alt="" class="img9 imgpc">
+    <br>
+    
+    <div  class="bt d-flex justify-content-center"><a href="https://iftm.edu.br/noticias/2024/10/03/professor-do-iftm-campus-patrocinio-participa-de-capacitacao-nacional-sobre-eficiencia-energetica/"><div><button type="button" class=" bt1 btn btn-success btpc"><p class="font35"  style="height:11px">Saber mais sobre</p></button></div></a></div>
+
+<br>
+<br>
+<?php include "../includes/footer.php"; ?>
+    <?php include "../includes/js.php"; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</div>
+</body>
+
+</html>
